8d14d0b5e2c5e9955002034949def10dd24bfa88
jluisferrer <jluisferrer@gmail.com>
2024-03-23 14:45:56 +0100

    feat:custom input structure created for register

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,9 +1,56 @@
+import { useState, useEffect } from "react";
+
 import "./Register.css"
+import { CInput } from "../../common/CInput/CInput";
 
 export const Register = () => {
+    const [user, setUser] = useState({
+        first_name: "",
+        last_name: "",
+        email: "",
+        password_hash: ""
+    })
+
+    //funcion emit que esta aqui en el padre.. se le pasa a custom input
+
+    const inputHandler = (e) => {
+        console.log(e.target.value)
+    }
+
     return (
         <div className="registerDesign">
-            soy la vista register
+            <CInput
+                className={"inputDesign"}
+                type={"text"}
+                placeholder={"first_name"}
+                name={"first_name"}
+                value={user.first_name || ""}
+                onChangeFunction={(e) => inputHandler(e)}
+            />
+            <CInput
+                className={"inputDesign"}
+                type={"text"}
+                placeholder={"last_name"}
+                name={"last_name"}
+                value={user.last_name || ""}
+                onChangeFunction={(e) => inputHandler(e)}
+            />
+            <CInput
+                className={"inputDesign"}
+                type={"email"}
+                placeholder={"email"}
+                name={"email"}
+                value={user.email || ""}
+                onChangeFunction={(e) => inputHandler(e)}
+            />
+            <CInput
+                className={"inputDesign"}
+                type={"password"}
+                placeholder={"password"}
+                name={"password_hash"}
+                value={user.password_hash || ""}
+                onChangeFunction={(e) => inputHandler(e)}
+            />
         </div>
     )
 }
