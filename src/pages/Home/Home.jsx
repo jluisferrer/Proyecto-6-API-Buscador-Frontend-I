824a6ec131f12e560a00ae27bc71123ca1736cf8
jluisferrer <jluisferrer@gmail.com>
2024-04-01 18:09:03 +0200

    fix: home text shadow and separate with paragraphs

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,11 +6,11 @@ export const Home = () => {
         <>
             <Header />
             <div className="homeDesign">
-                <p>Bienvenido a nuestro estudio de tatuajes, un espacio donde el arte y la individualidad se encuentran para crear expresiones únicas en la piel. Con una amplia gama de servicios, desde tatuajes personalizados hasta la restauración de trabajos antiguos, nos dedicamos a plasmar tu visión con la máxima precisión y cuidado. Nuestros artistas son expertos en convertir tu diseño o idea en una obra de arte permanente, utilizando técnicas avanzadas y los mejores materiales.
+                <p>Bienvenido a nuestro estudio de tatuajes, un espacio donde el arte y la individualidad se encuentran para crear expresiones únicas en la piel. Con una amplia gama de servicios, desde tatuajes personalizados hasta la restauración de trabajos antiguos, nos dedicamos a plasmar tu visión con la máxima precisión y cuidado. Nuestros artistas son expertos en convertir tu diseño o idea en una obra de arte permanente, utilizando técnicas avanzadas y los mejores materiales.</p>
 
-Ya sea que prefieras seleccionar un diseño de nuestro extenso catálogo o traer tu propia creación, te aseguramos un resultado que superará tus expectativas. Además, somos especialistas en la colocación de piercings y dilatadores, ofreciendo un servicio profesional y seguro para complementar tu estilo.
+                <p>Ya sea que prefieras seleccionar un diseño de nuestro extenso catálogo o traer tu propia creación, te aseguramos un resultado que superará tus expectativas. Además, somos especialistas en la colocación de piercings y dilatadores, ofreciendo un servicio profesional y seguro para complementar tu estilo.</p>
 
-En nuestro estudio, también encontrarás una exclusiva selección de piercings y artículos de arte corporal para venta, cuidadosamente elegidos para garantizar la calidad y satisfacer tus necesidades. Visítanos y deja que nuestro equipo te guíe en el camino hacia una nueva forma de expresión artística.</p>
+                En nuestro estudio, también encontrarás una exclusiva selección de piercings y artículos de arte corporal para venta, cuidadosamente elegidos para garantizar la calidad y satisfacer tus necesidades. Visítanos y deja que nuestro equipo te guíe en el camino hacia una nueva forma de expresión artística.
             </div>
         </>
 
