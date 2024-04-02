41baf71fe41069bc4f68f9276b1a74cb12edb76c
jluisferrer <jluisferrermartinez@gmail.com>
2024-04-02 21:22:04 +0200

    update: added checking whether the user has scheduled appointments or not

--- src/components/AppointmentsCard/AppoinmentsCard.jsx
+++ src/components/AppointmentsCard/AppoinmentsCard.jsx
@@ -3,9 +3,9 @@ import "./AppoinmentsCard.css"
 export const AppointmentsCard = ({ service_id, appointmentDate, appointmentId, onDelete }) => {
     return (
         <div className="appoinmentsCardDesign">
-            <div>Nombre del servicio:{service_id}</div>
-            <div>Fecha de la cita:{appointmentDate}</div>
-            <div>ID de la cita:{appointmentId}</div>
+            <div>Nombre del servicio: {service_id}</div>
+            <div>Fecha de la cita: {appointmentDate}</div>
+            <div>ID de la cita: {appointmentId}</div>
             <button className="deleteDesign" onClick={() => onDelete(appointmentId)}>Anular cita</button>
         </div>
     )
